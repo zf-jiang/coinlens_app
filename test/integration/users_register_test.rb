7bffd4c2ea6b1d3be28888149b756617830f6981
ZFJ <jack_jiangg@hotmail.com>
2018-11-03 21:53:34 -0700

    user registration test

--- test/integration/users_register_test.rb
+++ test/integration/users_register_test.rb
@@ -10,11 +10,23 @@ class UsersRegisterTest < ActionDispatch::IntegrationTest
 	count_after = User.count
 	assert_equal count_before, count_after
 =end
-  		post users_path, params: { user: { 	name: 					"",
-  										   	email: 					"foobar@invalid",
-  											password: 				"foo",
-  											password_confirmation: 	"bar" } }
+  	 post users_path, params: { user: { 	name: 					        "",
+  										   	                email: 					        "foobar@invalid",
+  											                  password: 				      "foo",
+  											                  password_confirmation: 	"bar" } }
   	end
   assert_template 'users/new' #returns to register page upon failed registration
+  end
+
+  test "valid registration" do
+    get register_path
+    assert_difference 'User.count', 1 do  #assertion: user count should increase by 1
+      post users_path, params: { user: {  name:                   "Foo Bar",
+                                          email:                  "foo@bar.com",
+                                          password:               "foobar",
+                                          password_confirmation:  "foobar" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'  #successful registration redirects to user profile
   end
 end
